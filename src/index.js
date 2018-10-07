b049b6075229f7800e4fb24498cc6f3152446419
igor-provotorov <garynya@yandex.ru>
2018-10-07 15:13:27 +0300

    smart-calculator/feature: complete task

--- src/index.js
+++ src/index.js
@@ -1,26 +1,81 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.arr = [];
+    this.arr.push(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.arr.push("+");
+    this.arr.push(number);
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.arr.push("-");
+    this.arr.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.arr.push("*");
+    this.arr.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.arr.push("/");
+    this.arr.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.arr.push("^");
+    this.arr.push(number);
+    return this;
+  }
+
+  valueOf() {
+    const resultMass = this.arr;
+    let length = this.arr.length;
+
+    for (let i = length; i > 0; i--) {
+      if (resultMass[i] === "^") {
+        let val = Math.pow(resultMass[i - 1], resultMass[i + 1]);
+        resultMass.splice(i - 1, 3, val);
+        length = resultMass.length;
+        i = length;
+     }
+    }
+
+    for (let i = 0; i < length; i++) {
+      if (resultMass[i] === "*") {
+        let val = resultMass[i - 1] * resultMass[i + 1];
+        resultMass.splice(i - 1, 3, val);
+         i = 0;
+        length = resultMass.length;
+      } else if (resultMass[i] === "/") {
+        let val = resultMass[i - 1] / resultMass[i + 1];
+        resultMass.splice(i - 1, 3, val);
+         i = 0;
+        length = resultMass.length;
+      }
+    }
+
+    for (let i = 0; i < length; i++) {
+      if (resultMass[i] === "+") {
+        let val = (+resultMass[i - 1]) + (+resultMass[i + 1]);
+        resultMass.splice(i - 1, 3, val);
+         i = 0;
+        length = resultMass.length;
+      } else if (resultMass[i] === "-") {
+        let val = resultMass[i - 1] - resultMass[i + 1];
+        resultMass.splice(i - 1, 3, val);
+         i = 0;
+        length = resultMass.length;
+      } 
+    }
+
+    return resultMass[0];
   }
 }
 
